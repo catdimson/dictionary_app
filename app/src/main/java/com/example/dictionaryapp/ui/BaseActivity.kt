1e3137adce20d752ad17b920c3be84fc22cb467c
Kotik <k-work31@yandex.ru>
2022-08-22 22:53:26 +0300

    lesson1. Добавлена разметка

---
+++ app/src/main/java/com/example/dictionaryapp/ui/BaseActivity.kt
@@ -0,0 +1,31 @@
+package com.example.dictionaryapp.ui
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.dictionaryapp.model.data.AppState
+import com.example.dictionaryapp.presenter.Presenter
+
+abstract class BaseActivity<T: AppState> : AppCompatActivity(), View {
+
+    protected lateinit var presenter: Presenter<T, View>
+
+    protected abstract fun createPresenter(): Presenter<T, View>
+
+    abstract override fun renderData(appState: AppState)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        presenter = createPresenter()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        presenter.attachView(this)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        presenter.detachView(this)
+    }
+}
