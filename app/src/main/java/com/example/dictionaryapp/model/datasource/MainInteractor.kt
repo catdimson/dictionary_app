e4e339120d92d33b7d0b4ba30d45283025522c7d
Kotik <k-work31@yandex.ru>
2022-08-23 00:49:21 +0300

    lesson1. Добавлен интерактор

---
+++ app/src/main/java/com/example/dictionaryapp/model/datasource/MainInteractor.kt
@@ -0,0 +1,21 @@
+package com.example.dictionaryapp.model.datasource
+
+import com.example.dictionaryapp.model.data.AppState
+import com.example.dictionaryapp.model.data.DataModel
+import com.example.dictionaryapp.model.repository.Repository
+import com.example.dictionaryapp.presenter.Interactor
+import io.reactivex.Observable
+
+class MainInteractor(
+    private val remoteRepository: Repository<List<DataModel>>,
+    private val localRepository: Repository<List<DataModel>>
+) : Interactor<AppState> {
+
+    override fun getData(word: String, fromRemoteSource: Boolean): Observable<AppState> {
+        return if (fromRemoteSource) {
+            remoteRepository.getData(word).map { AppState.Success(it) }
+        } else {
+            localRepository.getData(word).map { AppState.Success(it) }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/dictionaryapp/presenter/Interactor.kt
@@ -0,0 +1,9 @@
+package com.example.dictionaryapp.presenter
+
+import io.reactivex.Observable
+
+interface Interactor<T> {
+
+    fun getData(word: String, fromRemoteSource: Boolean): Observable<T>
+
+}
