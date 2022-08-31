08b4a1f74e814a860239be6158d8d95dca337220
Kotik <k-work31@yandex.ru>
2022-08-31 20:38:05 +0300

    lesson6. Теперь все константы вынесены в файл Constants и используются gradle-ом

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,14 +7,14 @@ plugins {
 }
 
 android {
-    compileSdk = 32
+    compileSdk = Config.compileSdk
 
     defaultConfig {
-        applicationId = "com.example.dictionaryapp"
-        minSdk = 26
-        targetSdk = 32
-        versionCode = 1
-        versionName = "1.0"
+        applicationId = Config.applicationId
+        minSdk = Config.minSdk
+        targetSdk = Config.targetSdk
+        versionCode = Releases.versionCode
+        versionName = Releases.versionName
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
@@ -32,8 +32,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = Config.javaVersion
+        targetCompatibility = Config.javaVersion
     }
     kotlinOptions {
         jvmTarget = "1.8"
@@ -47,38 +47,40 @@ android {
 dependencies {
 
     // Retrofit 2
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:0.9.2")
-    implementation("com.squareup.okhttp3:logging-interceptor:3.12.1")
+    implementation(Retrofit.retrofit)
+    implementation(Retrofit.converterGson)
+    implementation(Retrofit.retrofit2KotlinCoroutinesAdapter)
+
+    // Interceptor
+    implementation(Interceptor.loggingInterceptor)
 
     // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.1")
+    implementation(Coroutines.kotlinxCoroutinesAndroid)
 
-    //Koin
-    implementation("io.insert-koin:koin-core:3.1.2")
-    implementation("io.insert-koin:koin-android-compat:3.1.2")
-    implementation("io.insert-koin:koin-android:2.1.6")
-    implementation("androidx.lifecycle:lifecycle-viewmodel:2.5.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+    // Koin
+    implementation(Koin.koinCore)
+    implementation(Koin.koinAndroidCompat)
+    implementation(Koin.koinAndroid)
 
     // Room
-    kapt("androidx.room:room-compiler:2.4.3")
-    implementation("androidx.room:room-runtime:2.4.3")
-    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-    implementation("androidx.room:room-ktx:2.4.3")
+    implementation(Room.roomRuntime)
+    implementation(Room.swiperefreshlayout)
+    implementation(Room.roomKtx)
 
     // Coil
-    implementation("io.coil-kt:coil:0.11.0")
+    implementation(Coil.coil)
 
     // General
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.7.10")
-    implementation("androidx.appcompat:appcompat:1.5.0")
-    implementation("androidx.core:core-ktx:1.8.0")
-    implementation("com.google.android.material:material:1.6.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    kapt(General.roomCompiler)
+    implementation(General.lifecycleViewmodel)
+    implementation(General.lifecycleViewmodelKtx)
+    implementation(General.kotlinStdlibJdk7)
+    implementation(General.appcompat)
+    implementation(General.coreKtx)
+    implementation(General.material)
+    implementation(General.constraintlayout)
 
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    testImplementation(Tests.junit)
+    androidTestImplementation(Tests.exJunit)
+    androidTestImplementation(Tests.espressoCore)
 }
--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -1,2 +1,102 @@
-class Constants {
+import org.gradle.api.JavaVersion
+
+object Config {
+    const val applicationId = "com.example.dictionaryapp"
+    const val compileSdk = 32
+    const val minSdk = 26
+    const val targetSdk = 32
+    val javaVersion = JavaVersion.VERSION_1_8
+}
+
+object Releases {
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
+
+private object DependenciesVersion {
+    // Retrofit
+    const val retrofit = "2.9.0"
+    const val converterGson = "2.9.0"
+    const val retrofit2KotlinCoroutinesAdapter = "0.9.2"
+
+    // Interceptor
+    const val loggingInterceptor = "3.12.1"
+
+    // Coroutines
+    const val kotlinxCoroutinesAndroid = "1.6.1"
+
+    // Koin
+    const val koinCore = "3.1.2"
+    const val koinAndroidCompat = "3.1.2"
+    const val koinAndroid = "2.1.6"
+
+    // Room
+    const val roomRuntime = "2.4.3"
+    const val swiperefreshlayout = "1.1.0"
+    const val roomKtx = "2.4.3"
+
+    // Coil
+    const val coil = "0.11.0"
+
+    // General
+    const val roomCompiler = "2.4.3"
+    const val lifecycleViewmodel = "2.5.1"
+    const val lifecycleViewmodelKtx = "2.5.1"
+    const val kotlinStdlibJdk7 = "1.7.10"
+    const val appcompat = "1.5.0"
+    const val coreKtx = "1.8.0"
+    const val material = "1.6.1"
+    const val constraintlayout = "2.1.4"
+
+    // Tests
+    const val junit = "4.13.2"
+    const val exTjunit = "1.1.3"
+    const val espressoCore = "3.4.0"
+}
+
+object Retrofit {
+    const val retrofit = "com.squareup.retrofit2:retrofit:${DependenciesVersion.retrofit}"
+    const val converterGson = "com.squareup.retrofit2:converter-gson:${DependenciesVersion.converterGson}"
+    const val retrofit2KotlinCoroutinesAdapter = "com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:${DependenciesVersion.retrofit2KotlinCoroutinesAdapter}"
+}
+
+object Interceptor {
+    const val loggingInterceptor = "com.squareup.okhttp3:logging-interceptor:${DependenciesVersion.loggingInterceptor}"
+}
+
+object Coroutines {
+    const val kotlinxCoroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${DependenciesVersion.kotlinxCoroutinesAndroid}"
+}
+
+object Koin {
+    const val koinCore = "io.insert-koin:koin-core:${DependenciesVersion.koinCore}"
+    const val koinAndroidCompat = "io.insert-koin:koin-android-compat:${DependenciesVersion.koinAndroidCompat}"
+    const val koinAndroid = "io.insert-koin:koin-android:${DependenciesVersion.koinAndroid}"
+}
+
+object Room {
+    const val roomRuntime = "androidx.room:room-runtime:${DependenciesVersion.roomRuntime}"
+    const val swiperefreshlayout = "androidx.swiperefreshlayout:swiperefreshlayout:${DependenciesVersion.swiperefreshlayout}"
+    const val roomKtx = "androidx.room:room-ktx:${DependenciesVersion.roomKtx}"
+}
+
+object Coil {
+    const val coil = "io.coil-kt:coil:${DependenciesVersion.coil}"
+}
+
+object General {
+    const val roomCompiler = "androidx.room:room-compiler:${DependenciesVersion.roomCompiler}"
+    const val lifecycleViewmodel = "androidx.lifecycle:lifecycle-viewmodel:${DependenciesVersion.lifecycleViewmodel}"
+    const val lifecycleViewmodelKtx = "androidx.lifecycle:lifecycle-viewmodel-ktx:${DependenciesVersion.lifecycleViewmodelKtx}"
+    const val kotlinStdlibJdk7 = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${DependenciesVersion.kotlinStdlibJdk7}"
+    const val appcompat = "androidx.appcompat:appcompat:${DependenciesVersion.appcompat}"
+    const val coreKtx = "androidx.core:core-ktx:${DependenciesVersion.coreKtx}"
+    const val material = "com.google.android.material:material:${DependenciesVersion.material}"
+    const val constraintlayout = "androidx.constraintlayout:constraintlayout:${DependenciesVersion.constraintlayout}"
+}
+
+object Tests {
+    const val junit = "junit:junit:${DependenciesVersion.junit}"
+    const val exJunit = "androidx.test.ext:junit:${DependenciesVersion.exTjunit}"
+    const val espressoCore = "androidx.test.espresso:espresso-core:${DependenciesVersion.espressoCore}"
 }
